c5b92fabe8dc03422e566de01ffd9e535aabd004
duffbuster <cmackeydev@gmail.com>
2025-02-13 16:07:50 -0500

    tweak configs

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import pluginJs from '@eslint/js';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  ...pluginQuery.configs['flat/recommended'],
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
